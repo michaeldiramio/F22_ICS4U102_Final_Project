97ad97bd3abd1351c3787a9991ab2dad0f12620d
penninga <120403290+kennyIsKool@users.noreply.github.com>
2023-01-09 18:07:15 +0000

    Set up the basic player + minigame system

--- Minigame.java
+++ Minigame.java
@@ -1,6 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Minigame {
 
-	public void run() {
+  public void clear() {
+    //Clears the console
+    System.out.print("\033[H\033[2J"); 
+    System.out.flush();
+  }
+
+  public void wait(double amount) {
+    //For a custom pause
+    double miliseconds = amount * 1000;
+    int intify = (int) Math.round(miliseconds);
+    try {
+      Thread.sleep(intify);
+    } catch(Exception e) {
+    };
+  }
+
+  public void print(String phrase) {
+    //Lazy way of printing text
+    if (phrase != null) {
+      System.out.println(phrase);
+    } else {
+      System.out.println();
+    }
+  }
+
+  public void halfPrint(String phrase) {
+    //Lazy way of printing text
+    if (phrase != null) {
+      System.out.print(phrase);
+    } else {
+      System.out.print("");
+    }
+  }
+  
+  public String getMinigameName() {
+    return ("Title");
+  }
+  
+	public void run(ArrayList<Player> players) {
 		//Do the game here!
 	}
 }
---
+++ Player.java
@@ -0,0 +1,33 @@
+
+public class Player {
+  private String name;
+  private int number;
+  private int points = 0;
+
+  //this is a player
+  public Player(String name, int number, int points) {
+    this.name = name;
+    this.number = number;
+    this.points = points;
+  }
+
+  //getting points
+  public int getPoints() {
+    return this.points;
+  }
+
+  //getting number
+  public int getNumber() {
+    return this.number;
+  }
+
+  //getting name
+  public String getName() {
+    return this.name;
+  }
+  
+  //adding points
+  public void addPoints(int amount) {
+    this.points = this.points + amount;
+  }
+}
