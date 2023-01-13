2b0cba8d3d22afde240320224d540129b778cb19
penninga <120403290+kennyIsKool@users.noreply.github.com>
2023-01-13 15:24:32 +0000

    Initial commit

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -11,6 +11,7 @@ public class RockPaperScissors extends Minigame {
 	public void run(ArrayList<Player> players) {
     //Scanner
     Scanner userIn = new Scanner(System.in);
+    Random rdm = new Random();
     //Putting the players into a group
     ArrayList<Player> inGameP = new ArrayList<Player>();
     for (int i = 0; i < players.size(); i++) {
@@ -22,6 +23,17 @@ public class RockPaperScissors extends Minigame {
     while (!winnerFound) {
       //Splitting into teams
       String[] teams;
+      //Getting rid of anything that's null
+      for (int i = 0; i < inGameP.size(); i++) {
+        Player temp = inGameP.get(i);
+        if (temp == null) {
+          inGameP.remove(i);
+        }
+      }
+      //Is there only one player left?
+      if (inGameP.size() == 1) {
+        winnerFound = true;
+      }
       
       if (inGameP.size() % 2 != 0) {
         
@@ -55,23 +67,14 @@ public class RockPaperScissors extends Minigame {
         //Opponent 1 and 2
         String[] opponents = teams[i].split(",");
         String[] answers = new String[2];
-        //If bot is needed
-        if (opponents[1].equals("bot")) {
-          Player o1 = inGameP.get(Integer.parseInt(opponents[0]));
-          String o2 = "bot";
-
-          //Display
-          clear();
-          print("Rock, Paper, Scissors");
-          print("");
-          print("\t\t" + o1.getName() + " [P" + o1.getNumber() + "] VS bot");
-          print("");
-          //Player's answer
-          print(o1.getName() + "'s Turn");
-          print("Rock (1), Paper (2), or Scissors (3)?");
-          halfPrint(">");
-          int answer = userIn.nextInt();
-          while (answer != 1 && answer != 2 && answer != 3) {
+        Boolean tie = true;
+        while (tie) {
+          //If bot is needed
+          if (opponents[1].equals("bot")) {
+            Player o1 = inGameP.get(Integer.parseInt(opponents[0]));
+            String o2 = "bot";
+  
+            //Display
             clear();
             print("Rock, Paper, Scissors");
             print("");
@@ -79,50 +82,239 @@ public class RockPaperScissors extends Minigame {
             print("");
             //Player's answer
             print(o1.getName() + "'s Turn");
-            print("INVALID ANSWER!");
-            print("Rock (1), Paper (2), or Scissors (3)?");
+            print("Rock (1),\tPaper (2),\tor\tScissors (3)?");
             halfPrint(">");
-            answer = userIn.nextInt();
-          }
-          if (answer == 1) {
-            answer[0] = "Rock O";
-          } else if (answer == 2) {
-            answer[0] = "Paper _";
-          } else {
-            answer[0] = "Scissors 8<";
-          }
-          //Bot's answer
+            int answer = userIn.nextInt();
+            while (answer != 1 && answer != 2 && answer != 3) {
+              clear();
+              print("Rock, Paper, Scissors");
+              print("");
+              print("\t\t" + o1.getName() + " [P" + o1.getNumber() + "] VS bot");
+              print("");
+              //Player's answer
+              print(o1.getName() + "'s Turn");
+              print("INVALID ANSWER!");
+              print("Rock (1),\tPaper (2),\tor\tScissors (3)?");
+              halfPrint(">");
+              answer = userIn.nextInt();
+            }
+            if (answer == 1) {
+              answers[0] = "O"; //Rock
+            } else if (answer == 2) {
+              answers[0] = "_"; //Paper
+            } else {
+              answers[0] = "*<"; //Scissors
+            }
+            clear();
+            //Bot's answer
             print("Rock, Paper, Scissors");
             print("");
             print("\t\t" + o1.getName() + " [P" + o1.getNumber() + "] VS bot");
             print("");
             //Player's answer
             print("bot's Turn");
-            print("Rock (1), Paper (2), or Scissors (3)?");
+            print("Rock (1),\tPaper (2),\tor\tScissors (3)?");
             print("");
             halfPrint("Bot is thinking");
             
-            wait(1.0);
+            wait(0.5);
             halfPrint(".");
-            wait(1.0);
+            wait(0.5);
             halfPrint(".");
-            wait(1.0);
+            wait(0.5);
             halfPrint(".");
-            wait(1.0);
+            wait(0.5);
+            answer = rdm.nextInt(3) + 1;
+            if (answer == 1) {
+              answers[1] = "O"; //Rock
+            } else if (answer == 2) {
+              answers[1] = "_"; //Paper
+            } else {
+              answers[1] = ">8"; //Scissors
+            }
             clear();
-          
-          
-        } else {
-          Player o1 = inGameP.get(Integer.parseInt(opponents[0]));
-          Player o2 = inGameP.get(Integer.parseInt(opponents[1]));
+            //Animation
+            for (int j = 0; j < 5; j++) {
+              print("Rock, Paper, Scissors");
+              print("");
+              print(o1.getName() + "\t\t\tbot");
+              print("");
+              if (j % 2 != 0) {
+                print("");
+                print("O\t\t\tO");
+              } else {
+                print("O\t\t\tO");
+                print("");
+              }
+              wait(0.5);
+              clear();
+            }
+            print("Rock, Paper, Scissors");
+            print("");
+            print(o1.getName() + "\t\t\tbot");
+            print("");
+            print("");
+            print(answers[0] + "\t\t\t" + answers[1]);
+            wait(1.5);
+            if (answers[0].equals(answers[1])) {
+              //If tied
+              print("TIE! Go again.");
+              wait(1.0);
+              //Restarts
+            } else if ((answers[0].equals("O") && answers[1].equals(">8")) || (answers[0].equals("8<") && answers[1].equals("_")) || (answers[0].equals("_") && answers[1].equals("O"))) {
+              //If O1 wins
+              print(o1.getName() + " WINS!");
+              wait(1.0);
+              tie = false;
+            } else {
+              //If bot wins
+              print("bot WINS!");
+              wait(1.0);
+              inGameP.set(Integer.parseInt(opponents[0]), null);
+              tie = false;
+            }
+            wait(1.0);
+          } else {
+            //If there is no bot
+            Player o1 = inGameP.get(Integer.parseInt(opponents[0]));
+            Player o2 = inGameP.get(Integer.parseInt(opponents[1]));
+
+            //Display
+            clear();
+            print("Rock, Paper, Scissors");
+            print("");
+            print("\t\t" + o1.getName() + " [P" + o1.getNumber() + "] VS " + o2.getName() + " [P" + o2.getNumber() + "]");
+            print("");
+            //O1's answer
+            print(o1.getName() + "'s Turn");
+            print("Rock (1),\tPaper (2),\tor\tScissors (3)?");
+            print("");
+            print("[Don't let the other player see your answer!]");
+            halfPrint(">");
+            int answer = userIn.nextInt();
+            while (answer != 1 && answer != 2 && answer != 3) {
+              clear();
+              print("Rock, Paper, Scissors");
+              print("");
+              print("\t\t" + o1.getName() + " [P" + o1.getNumber() + "] VS bot" + o2.getName() + " [P" + o2.getNumber() + "]");
+              print("");
+              //Player's answer
+              print(o1.getName() + "'s Turn");
+              print("INVALID ANSWER!");
+              print("Rock (1),\tPaper (2),\tor\tScissors (3)?");
+              print("");
+              print("[Don't let the other player see your answer!]");
+              halfPrint(">");
+              answer = userIn.nextInt();
+            }
+            if (answer == 1) {
+              answers[0] = "O"; //Rock
+            } else if (answer == 2) {
+              answers[0] = "_"; //Paper
+            } else {
+              answers[0] = "8<"; //Scissors
+            }
+            clear();
+            //O2's answer
+            print("Rock, Paper, Scissors");
+            print("");
+            print("\t\t" + o1.getName() + " [P" + o1.getNumber() + "] VS "+ o2.getName() + " [P" + o2.getNumber() + "]");
+            print("");
+            print(o2.getName() + "'s Turn");
+            print("Rock (1),\tPaper (2),\tor\tScissors (3)?");
+            print("");
+            print("[Don't let the other player see your answer!]");
+            halfPrint(">");
+            answer = userIn.nextInt();
+            while (answer != 1 && answer != 2 && answer != 3) {
+              clear();
+              print("Rock, Paper, Scissors");
+              print("");
+              print("\t\t" + o1.getName() + " [P" + o1.getNumber() + "] VS bot");
+              print("");
+              //Player's answer
+              print(o1.getName() + "'s Turn");
+              print("INVALID ANSWER!");
+              print("Rock (1),\tPaper (2),\tor\tScissors (3)?");
+              print("");
+              print("[Don't let the other player see your answer!");
+              halfPrint(">");
+              answer = userIn.nextInt();
+            }
+            if (answer == 1) {
+              answers[1] = "O"; //Rock
+            } else if (answer == 2) {
+              answers[1] = "_"; //Paper
+            } else {
+              answers[1] = ">8"; //Scissors
+            }
+            clear();
+            //Animation
+            for (int j = 0; j < 5; j++) {
+              print("Rock, Paper, Scissors");
+              print("");
+              print(o1.getName() + "\t\t\t" + o2.getName());
+              print("");
+              if (j % 2 != 0) {
+                print("");
+                print("O\t\t\tO");
+              } else {
+                print("O\t\t\tO");
+                print("");
+              }
+              wait(0.5);
+              clear();
+            }
+            print("Rock, Paper, Scissors");
+            print("");
+            print(o1.getName() + "\t\t\t" + o2.getName());
+            print("");
+            print("");
+            print(answers[0] + "\t\t\t" + answers[1]);
+            wait(1.5);
+            if (answers[0].equals(answers[1])) {
+              //If tied
+              print("TIE! Go again.");
+              wait(2.0);
+              //Restarts
+            } else if ((answers[0].equals("O") && answers[1].equals(">8")) || (answers[0].equals("8<") && answers[1].equals("_")) || (answers[0].equals("_") && answers[1].equals("O"))) {
+              //If O1 wins
+              print(o1.getName() + " WINS!");
+              wait(2.0);
+              inGameP.set(Integer.parseInt(opponents[1]), null);
+              tie = false;
+            } else {
+              //If bot wins
+              print(o2.getName() +" WINS!");
+              wait(2.0);
+              inGameP.set(Integer.parseInt(opponents[0]), null);
+              tie = false;
+            }
+          }
         }
-        
-        
-        
-        
       }
-      wait(5.0);
+      for (int i = 0; i < inGameP.size(); i++) {
+        Player temp = inGameP.get(i);
+        if (temp == null) {
+          inGameP.remove(i);
+        }
+      }
+      if (inGameP.size() == 1) {
+        winnerFound = true;
+      }
+      wait(1.0);
     }
+
+    clear();
+    print("Rock, Paper, Scissors");
+    print("");
+    print(inGameP.get(0).getName() + " IS THE WINNER!");
+    wait(1.0);
+    print("+2 points");
+    inGameP.get(0).addPoints(2);
+    wait(1.0);
+    print("They now have " + inGameP.get(0).getPoints() + " points");
+    wait(3.0);
     
 	}
 }
