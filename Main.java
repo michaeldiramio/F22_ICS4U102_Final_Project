7e0d8558973c88f41db1d71161a16657562645d0
penninga <120403290+kennyIsKool@users.noreply.github.com>
2023-01-17 16:09:51 +0000

    Added the wordle minigame, corrected typos in the rock-paper-scissors minigame, and made sure minigames cannot be repeated in the main loop.

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Main {
   private ArrayList<Player> players = new ArrayList<Player>();
-  private Minigame[] minigames = new Minigame[1];
+  private Minigame[] minigames = new Minigame[2];
   private Scanner userIn;
   private Random rdm = new Random();
 
@@ -46,7 +46,8 @@ public class Main {
     //----------------------
     //SPECIFY MINIGAMES HERE!
 
-    minigames[0] = new RockPaperScissors();
+    minigames[0] = new Wordle();
+    minigames[1] = new RockPaperScissors();
   
     //----------------------
     
@@ -150,6 +151,7 @@ public class Main {
       clear();
       //Running the game
       minigames[random].run(players);
+      minigames[random] = null;
     }
 
     //Finding the winner
